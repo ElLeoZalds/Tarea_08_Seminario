1af62eb76804a0213c3daffd638b4b30c248980d
Leonardo <leonardoalexander130@gmail.com>
2026-05-31 20:19:07 -0500

    Operaciones de suma y multiplicación realizadas

--- app/src/main/java/com/example/tarea_08/fragments/Ejercicio01Fragment.java
+++ app/src/main/java/com/example/tarea_08/fragments/Ejercicio01Fragment.java
@@ -6,26 +6,40 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-
 import com.example.tarea_08.R;
-
 import org.w3c.dom.Text;
 
-
 public class Ejercicio01Fragment extends Fragment {
-
-
-    public Ejercicio01Fragment() {
-    }
+    private EditText edt1, edt2;
+    private TextView txtRespuesta;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        // Asociar el fragment con el XML
-        return inflater.inflate(R.layout.activity_tarea01, container, false);
+    // Asociar el fragment con el XML
+        View v = inflater.inflate(R.layout.activity_tarea01, container, false);
+
+        edt1 = v.findViewById(R.id.edtNum1);
+        edt2 = v.findViewById(R.id.edtNum2);
+        txtRespuesta = v.findViewById(R.id.txtRespuesta);
+
+        v.findViewById(R.id.btnRespuesta).setOnClickListener(view -> calcular());
+
+        return v;
+    }
+    private void calcular() {
+        try {
+            double suma = Double.parseDouble(edt1.getText().toString()) +
+                    Double.parseDouble(edt2.getText().toString());
+
+            txtRespuesta.setText("La respuesta es: " + suma);
+        } catch (NumberFormatException e) {
+            txtRespuesta.setText("Error: Ingrese números");
+        }
     }
 }
--- app/src/main/java/com/example/tarea_08/fragments/Ejercicio02Fragment.java
+++ app/src/main/java/com/example/tarea_08/fragments/Ejercicio02Fragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -13,14 +15,30 @@ import com.example.tarea_08.R;
 
 public class Ejercicio02Fragment extends Fragment {
 
-    public Ejercicio02Fragment() {
-
-    }
+    private EditText edt1, edt2;
+    private TextView txtRespuesta;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         // Asociar el fragment con el XML
-        return inflater.inflate(R.layout.activity_tarea02, container, false);
+        View v = inflater.inflate(R.layout.activity_tarea01, container, false);
+
+        edt1 = v.findViewById(R.id.edtNum1);
+        edt2 = v.findViewById(R.id.edtNum2);
+        txtRespuesta = v.findViewById(R.id.txtRespuesta);
+
+        v.findViewById(R.id.btnRespuesta).setOnClickListener(view -> calcular());
+
+        return v;
     }
-}
+    private void calcular() {
+        try {
+            double suma = Double.parseDouble(edt1.getText().toString()) *
+                    Double.parseDouble(edt2.getText().toString());
+
+            txtRespuesta.setText("La respuesta es: " + suma);
+        } catch (NumberFormatException e) {
+            txtRespuesta.setText("Error: Ingrese números");
+        }
+    }}
